28acace32368ac7655fcf2a9e7cb7f8213ad02b6
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:51:16 +0100

    Update: AUTOSAR basic software service network management module for CAN communication protocol stack

---
+++ main_R2023/infServiceCanNm_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceCanNm_ServiceNvM.hpp                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICECANNM_AR_RELEASE_VERSION_MAJOR                             4
+#define CONSTSERVICECANNM_AR_RELEASE_VERSION_MINOR                             3
+
+#define Const_DefaultInit_ConstServiceCanNm                                                                                \
+   ,  {CONSTSERVICECANNM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICECANNM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
