4119383910be4c9c6cc3d0932169e0820a1e16cc
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:30:17 +0100

    Update: AUTOSAR basic software service network management module for CAN communication protocol stack

--- main_R2023/infServiceCanNm_ServiceNvM_Types.hpp
+++ main_R2023/infServiceCanNm_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceCanNm_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceCanNm_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
